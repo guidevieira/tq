8691c2528c8f1f7bd6aee8b02322fde52e35a8e9
guilerme <guilerme1041@gmail.com>
2021-03-16 12:59:37 -0300

    up build

---
+++ build/precache-manifest.2f9568f932202205e49ad4d58b1ce95d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c25c136f198ee73c087025da4faaba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb4b40204fd2004781e",
+    "url": "/static/css/2.b3876bf4.chunk.css"
+  },
+  {
+    "revision": "5d09af82b7d23cde16f5",
+    "url": "/static/css/main.b4f4a5b7.chunk.css"
+  },
+  {
+    "revision": "cfb4b40204fd2004781e",
+    "url": "/static/js/2.db0d6eaa.chunk.js"
+  },
+  {
+    "revision": "5d09af82b7d23cde16f5",
+    "url": "/static/js/main.541d7af5.chunk.js"
+  },
+  {
+    "revision": "9dd97983d5935a0431f9",
+    "url": "/static/js/runtime~main.868d999b.js"
+  },
+  {
+    "revision": "00afc830e9d71e86d88e2e709ab68faa",
+    "url": "/static/media/1.00afc830.jpg"
+  },
+  {
+    "revision": "841cca7d6981e69440839002d9d2e0da",
+    "url": "/static/media/1.841cca7d.jpg"
+  },
+  {
+    "revision": "767f0bbb7218666a1fe2fee8e7f83e1f",
+    "url": "/static/media/3.767f0bbb.jpg"
+  },
+  {
+    "revision": "841cca7d6981e69440839002d9d2e0da",
+    "url": "/static/media/3.841cca7d.jpg"
+  },
+  {
+    "revision": "767f0bbb7218666a1fe2fee8e7f83e1f",
+    "url": "/static/media/4.767f0bbb.jpg"
+  },
+  {
+    "revision": "841cca7d6981e69440839002d9d2e0da",
+    "url": "/static/media/4.841cca7d.jpg"
+  },
+  {
+    "revision": "2770d87181549546bcf430076d152572",
+    "url": "/static/media/7.2770d871.jpg"
+  },
+  {
+    "revision": "32ce4100989a7a014f8433cb8c2281a2",
+    "url": "/static/media/bg.32ce4100.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "44a01ec957f53567c070ebb2383b3d82",
+    "url": "/static/media/logo_tq.44a01ec9.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
